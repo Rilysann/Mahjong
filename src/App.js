89e0c0165ee89f2f330406e2c6726129f0795a6e
Rilysann <migornak@gmail.com>
2022-11-08 17:33:10 +0200

    Remove inline styles from title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import Cards from "./components/Cardsfield";
 
 function App() {
   return (
     <div className="App">
-      <h1 style={{textAlign: 'center', fontSize: '48px', marginTop: '48px'}} className="title">Mahjong</h1>
+      <h1 className="title">Mahjong</h1>
       <Cards />
     </div>
   );
